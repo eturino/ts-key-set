00996bf9aab542aaaa2c3b3d9045003da41b61b0
Eduardo Turiño <eturino@plandek.com>
2019-09-07 01:29:29 +0100

    expose KeySet

--- src/lib/key-set.ts
+++ src/lib/key-set.ts
@@ -1,9 +1,11 @@
+import { KeySet } from './key-set/-base';
 import { all, KeySetAll } from './key-set/all';
 import { allExceptSome, KeySetAllExceptSome } from './key-set/all-except-some';
 import { KeySetNone, none } from './key-set/none';
 import { KeySetSome, some } from './key-set/some';
 
 export {
+  KeySet,
   all,
   allExceptSome,
   KeySetAll,
